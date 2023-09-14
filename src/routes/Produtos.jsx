1c2275b010035e625ea7494ceaaa88f0fe744e06
rafak7 <rafael.linoseabra@gmail.com>
2023-09-13 21:58:25 -0300

    Aula 13/09

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,50 +1,64 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import {AiFillEdit as Editar} from "react-icons/ai";
-import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
-
+import { AiFillEdit as Editar } from "react-icons/ai";
+import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
+import styles from "./Produtos.module.css";
 
 export default function Produtos() {
-
   document.title = "Produtos";
 
-    const estiloTabela = {
-      borderCollapse: "collapse",
-      margin:"0 auto",
-      border: "2px solid #ccc",
-    }
-  
   return (
     <>
-      <h1>Produtos</h1>
+      <h1>Lista de Produtos</h1>
 
       <div>
+        <table className={styles.tblEstilo}>
 
-        <table style={estiloTabela}>
-          <tr>
-            <th>ID</th>
-            <th>NOME</th>
-            <th>DESCRIÇÃO</th>
-            <th>IMAGEM</th>
-            <th>PREÇO</th>
-            <th>EDITAR/EXCLUIR</th>
-          </tr>
-          
-          {ListaProdutos.map( (item, indice)=>(
-            <tr key={indice}>
-              <td>{item.id}</td>
-              <td>{item.nome}</td>
-              <td>{item.desc}</td>
-              <td><img src={`${item.img}`} alt={`${item.desc}`} /></td>
-              <td>{item.preco}</td>
-              <td> <Link to={`/editar/produtos/${item.id}`}> <Editar> </Editar></Link> / <Link to={`/excluir/produtos/${item.id}`}>Excluir</Link></td>
+          <thead className={styles.tblHeader}>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>DESCRIÇÃO</th>
+              <th>IMAGEM</th>
+              <th>PREÇO</th>
+              <th>EDITAR/EXCLUIR</th>
             </tr>
-          ))}
+          </thead>
+
+          <tbody>
+            {ListaProdutos.map((item, indice) => (
+              <tr key={indice} className={styles.tblRow}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>
+                  <img src={`${item.img}`} alt={`${item.desc}`} />
+                </td>
+                <td>{item.preco}</td>
+                <td>
+                  {" "}
+                  <Link to={`/editar/produtos/${item.id}`}>
+                    {" "}
+                    <Editar />{" "}
+                  </Link>{" "}
+                  /{" "}
+                  <Link to={`/excluir/produtos/${item.id}`}>
+                    {" "}
+                    <Excluir />{" "}
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+          <tfoot>
+            <tr>
+              <td colSpan={6}>PRODUTOS</td>
+            </tr>
+          </tfoot>
           
-
         </table>
-
       </div>
     </>
-  )
+  );
 }
